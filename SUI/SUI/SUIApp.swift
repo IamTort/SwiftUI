ef8fc9f941532cba11120b4b76efb1bb508fcf89
IamTort <61164603+IamTort@users.noreply.github.com>
2023-01-17 12:40:20 +0300

    комит

---
+++ SUI/SUI/SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUIApp.swift
+//  SUI
+//
+//  Created by angelina on 17.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
