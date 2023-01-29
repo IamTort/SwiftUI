37766e6dcc2b253b56366716e447a87e178704f9
IamTort <61164603+IamTort@users.noreply.github.com>
2023-01-30 02:59:43 +0300

    .

--- SUI/SUI/ShakeGeometryEffect.swift
+++ SUI/SUI/ShakeGeometryEffect.swift
@@ -5,14 +5,25 @@ import SwiftUI
 
 /// Эффект дрожания
 struct ShakeGeometryEffect: GeometryEffect {
-    var amount: CGFloat = 10
-    var shakesPerUnit = 3
-    var animatableData: CGFloat
+    
+    // MARK: - Private Constants
 
+    private enum Constants {
+        static let amountNumber: CGFloat = 10
+        static let shakesPerUnitNumber = 3
+        static let defaultExpireMonthName = "05"
+        static let defaultExpireYearName = "20"
+        static let emptyString = ""
+    }
+    
+    var amount: CGFloat = Constants.amountNumber
+    var shakesPerUnit = Constants.shakesPerUnitNumber
+    var animatableData: CGFloat
+    
     func effectValue(size: CGSize) -> ProjectionTransform {
         ProjectionTransform(CGAffineTransform(
             translationX:
-            amount * sin(animatableData * .pi * CGFloat(shakesPerUnit)),
+                amount * sin(animatableData * .pi * CGFloat(shakesPerUnit)),
             y: 0
         ))
     }
